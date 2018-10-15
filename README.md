dbd573177f5c73ff37a714e2f597434493b11fa0
João Fernandes <etnepres@gmail.com>
2018-10-15 22:35:07 +0100

    Readme update, natural_readers.user.js added

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# trophymanager-super-voice-report
-Trophymanager Super Voice Report live and relive games
+# Trophymanager SVR
+
+Now with Super Voice Reports you can just relax and listen to your game reports.
+
+This *userscript* uses external services with optional premium plans, if you like the server feel free to use premium but keep in mind **I get nothing for those purchases** and I dont control if the services fails in some way.
+
+For now you can only choose one script:
+
+[**natural_readers.user.js**](https://github.com/etnepres/trophymanager-super-voice-report/blob/master/natural_readers.user.js) uses [Natural Readers](https://www.naturalreaders.com/)
+
+*Premium voices have as a total **20 minutes free per day**, that will most likely be **enough for a full match report**, just save your free minutes for the live matches.*
+
+- Available **[Free]** languages
+    - English (Zira Desktop) (US)
+- Available **[Free for 20 Minutes]** per day Or [Premium](https://www.naturalreaders.com/webapp.html#upgrade)
+    - English (US)
+    - English (UK)
+    - Spanish (Spain)
+    - Spanish (Mexico)
+    - Spanish (Castilian)
+    - Spanish (US)
+    - French
+    - French (Canada)
+    - German
+    - Italian
+    - Portuguese (Portugal)
+    - Portuguese (Brazil)
+    - Swedish
+    - Dutch
+
+## How to install
+
+This script was made, and tested on **chrome**. You can try install or using on other browsers but keep in mind its untested.
+
+### Chrome
+
+1. If you don't have already install extension capable of running userscript such has [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en), it may require you to restart chrome afterwards.
+
+2. Go to diz link of yoru choice, example: [**natural_readers.user.js**](https://github.com/etnepres/trophymanager-super-voice-report/blob/master/natural_readers.user.js) 
+
+3. On page *trophymanager-super-voice-report/natural_readers.user.js* click Buttom [**Raw**](https://raw.githubusercontent.com/etnepres/trophymanager-super-voice-report/master/natural_readers.user.js)
+
+4. [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en) should now be opened, press install and go on test it out.
+
+5. Don't forget to leave me a start if you like it. Good luck and good gaming.
---
+++ natural_readers.user.js
@@ -0,0 +1,414 @@
+// ==UserScript==
+// @name         Trophymanager Super Voice Reports
+// @namespace    http://trophymanager.com/
+// @version      1.0
+// @description  Now with Super Voice Reports you can just relax and listen to your game reports.
+// @match        https://trophymanager.com/matches/*
+// @match        https://www.naturalreaders.com/online/
+// @author       Joao Manuel Ferreira Fernandes
+// @github       https://github.com/etnepres/trophymanager-super-voice-report
+// @grant        none
+// ==/UserScript==
+
+// ADDED
+setInterval(function(){
+    jQuery(document).attr("title", "HOME" + " ["+$("#main > div.top_team_bars > div.abs.score > div > span.home").html() + " - " + $("#main > div.top_team_bars > div.abs.score > div > span.away").html()+"] "+"AWAY");
+}, 5000);
+// ------- END ADDED
+
+// https://trophymanager.com/js/matchviewer.2.0.js?v=3.016.1
+function prepare_next_minute() {
+
+    // ADDED
+    function escapeRegExp(str) {
+        return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+    }
+
+    function replaceAll(str, find, replace) {
+        return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+    }
+
+    function getPlayerlastName(str, p1) {
+
+        if (p1 != null) {
+
+            var playerActual = allPlayers[p1];
+
+            if (playerActual != null && typeof playerActual["nameLast"] != "undefined") {
+                return allPlayers[p1]["nameLast"];
+            }
+        }
+    }
+    // ------- END ADDED
+
+    match_next_minute = get_next_minute_with_severity(match_last_min);
+
+    if (match_next_minute !== false && !test_prepared_minute(match_next_minute)) {
+
+        console.log("Prepareing next minute - " + match_next_minute);
+
+        // Add minute as has been prepared
+        match_prepared_minutes.push(match_next_minute);
+
+        // Merge all videos for the loader and prepare text boxes
+        var video_list = [];
+
+        for (var i in report[match_next_minute]) {
+
+            if (settings["severity"] == 0 || (settings["severity"] == 1 && report[match_next_minute][i]["severity"] == 1)) {
+
+                var entry = report[match_next_minute][i];
+
+                // ADDED
+                var allPlayers = [];
+                for (var attr in lineup["home"]) { allPlayers[attr] = lineup["home"][attr]; }
+                for (var attr in lineup["away"]) { allPlayers[attr] = lineup["away"][attr]; }
+                var relato = "";
+
+                for (var j = 0; j < entry["chance"]["text"].length; j++) {
+
+                    var relatoActualArray = entry["chance"]["text"][j];
+                    var relatoActual = "";
+                    for (var k = 0; k < relatoActualArray.length; k++) {
+                        relatoActual = relatoActual + relatoActualArray[k];
+                        if (!relatoActual.endsWith(".") && !relatoActual.endsWith("!") && !relatoActual.endsWith("?")) {
+                            relatoActual = relatoActual + " ";
+                        }
+                        relatoActual = relatoActual + " ";
+                    }
+
+                    relatoActual = relatoActual.replace(/\[player=(\d+)\]/gi, getPlayerlastName);
+                    relatoActual = relatoActual.replace("[goal]", "");
+                    relatoActual = relatoActual.replace("[sub]", "");
+                    relatoActual = replaceAll(relatoActual, "GOAL", "Goal");
+                    relatoActual = replaceAll(relatoActual, "GOOOOOOOOOAL", "Goal");
+                    relatoActual = replaceAll(relatoActual, "GOOOOOOOOOOLO", "Golo");
+                    relatoActual = replaceAll(relatoActual, "GOLOOOOOO", "Golo");
+                    relatoActual = replaceAll(relatoActual, "(clap-clap-clap)", "");
+
+
+
+                    if (!relatoActual.endsWith(".") && !relatoActual.endsWith("!") && !relatoActual.endsWith("?")) {
+                        relatoActual = relatoActual + " ";
+                    }
+
+                    relato = relato + relatoActual + "\n";
+
+                    relato = relato.replace(". .", "");
+
+                }
+
+                if (typeof match_data["report_relato"] == "undefined") {
+                    match_data["report_relato"] = [];
+                }
+
+                if (typeof match_data["report_relato"][match_next_minute] == "undefined") {
+                    match_data["report_relato"][match_next_minute] = relato;
+                } else {
+                    match_data["report_relato"][match_next_minute] += " " + relato;
+                }
+                // ------- END ADDED
+
+                // Prune action text holders
+                var action_text_holders = $(".actiontext .scroller .entry");
+                if (action_text_holders.size() > 6) action_text_holders.first().remove();
+                var new_margin = 266 - $(".scroller").outerHeight();
+                $(".scroller").css({ "margin-top": new_margin });
+
+                // Add time to entry
+                entry["time"] = match_next_minute;
+
+                // Make new action text holder
+                $(".actiontext .scroller").append(template_action_holder(entry, flash_status["enabled"]));
+
+                // Add videos to video list
+                if (entry["chance"]["video"]) video_list = video_list.concat(entry["chance"]["video"]);
+
+            }
+            //match_pause();
+        }
+
+        if (video_list.length > 0 && flash_status["enabled"] && $("#viewer")[0] && $("#viewer")[0].gateway) {
+
+            flash_start_loaded_minutes_ar.push(match_next_minute);
+
+            // Cashe fix, treats duplicates as unique clips, otherwise animation complete will not fire.
+            for (var i in video_list) {
+                for (var j in video_list) {
+                    if (i != j) {
+                        if (video_list[i]["video"] == video_list[j]["video"]) {
+                            video_list[i]["video"] = video_list[i]["video"] + "?" + i;
+                        }
+                    }
+                }
+            }
+
+            // try {
+            load_clips(video_list, match_next_minute);
+
+            // } catch(e) {
+            // 	console.log("CATCHY");
+            // 	flash_status["enabled"] = false;
+            // 	flash_finished_loaded_minutes_ar.push(match_next_minute);
+            // 	flash_played_minutes_ar.push(match_next_minute);
+
+            // 	// Make sure report is running
+            // 	if (!flash_status["report_started"]) start_report();
+
+            // }
+
+        } else {
+
+            flash_finished_loaded_minutes_ar.push(match_next_minute);
+            flash_played_minutes_ar.push(match_next_minute);
+
+            // Make sure report is running
+            if (!flash_status["report_started"]) start_report();
+
+        }
+    }
+}
+
+//https://trophymanager.com/js/matchviewer.2.0.js?v=3.016.1
+function run_match() {
+
+    // ADDED
+    if (typeof match_data["report_done"] == "undefined") {
+        match_data["report_done"] = [-1, 0];
+    }
+
+    if (!flash_status["loaded_main"] && match_data["report_done"].length == 3 && match_data["report_done"].indexOf(match_next_minute) == -1){// && match_data["report_done"].indexOf(match_next_minute) == -1) { //  && match_data["report_done"].length == 2
+        setTimeout(function(){
+            match_data["report_done"].push(match_next_minute);
+        },2000);
+    }
+
+    if (match_time_mode == "live" && flash_status["loaded_main"] && match_data["report_done"].indexOf(match_next_minute) == -1 && flash_finished_loaded_minutes_ar.indexOf(match_next_minute) > -1) { //  && match_data["report_done"].length == 2
+        setTimeout(function(){
+            match_data["report_done"].push(match_next_minute);
+        },2000);
+    }
+    // ------- END ADDED
+
+    var time = timeObj(match_seconds);
+
+    // Stop if end of game
+    if (end_of_game) return;
+    // End at end of game
+    if (time.m >= end_game && !flash_playing_clip && !$(".actiontext .scroller p:hidden").length > 0) {
+        match_end();
+        return;
+    }
+
+    // Paused? Wait a sec and check for unpaused then
+    if (flash_status["playback_mode"] == "pause") {
+        setTimeout(run_match, 1000);
+        return false;
+    }
+
+
+    // Update clock
+    //$(".clock").text(time.m_leading+":"+time.s_leading);
+    $(".clock").text(get_display_time(match_seconds).formatted_s);
+
+    // Start half time
+    if (time.m == 45 && match_half_time === false && match_time_mode == "live") {
+
+        match_half_time = 0;
+        play_refblow();
+        setTimeout(run_match, 1000);
+        return false;
+
+        // Continue half time
+    } else if (time.m == 45 && match_half_time <= 900 && match_time_mode == "live") {
+
+        var half_time = timeObj(900 - match_half_time);
+        $(".ht_clock").show().text(half_time.m_leading + ":" + half_time.s_leading);
+
+        match_half_time++;
+
+        // Hide clock
+        if (match_half_time >= 900) $(".ht_clock").hide();
+
+        setTimeout(run_match, 1000);
+        return false;
+
+    } else if (time.m >= 45 && match_half_time === false && match_time_mode == "relive") {
+        match_half_time = true;
+        element_venue.update();
+    }
+
+
+    // No flash
+    if (!flash_status["enabled"]) {
+        if ($(".scroller p:hidden").size() > 0 && time.m >= match_next_minute) {
+            show_next_action_text_entry();
+        } else {
+            finish_playing(0);
+        }
+    }
+
+    // Match still going on
+
+    if (match_data["report_done"].indexOf(Math.floor(match_seconds / 60)) > -1) { // ADDED - LINE - IF
+
+        if (match_last_min != Math.floor(match_seconds / 60)) {
+
+            // New minute called
+            match_last_min = Math.floor(match_seconds / 60);
+            console.log("New minute called: " + match_last_min);
+
+            update_stats(true);
+            update_elements_with_stats(false);
+            update_live_elements();
+
+            if (report[match_last_min]) {
+
+                play_clips(match_last_min);
+            }
+
+            var iframe = document.getElementById('frame1'); // ADDED - LINE
+            iframe.contentWindow.postMessage('narrate:' + match_data["report_relato"][match_last_min], 'https://www.naturalreaders.com'); // ADDED - LINE
+
+        }
+    } // ADDED - LINE - END IF
+
+
+    // *** Bump time ***
+    // Next minute loaded and current minute playback done speed up clock
+    // CHANGED LINE - WAS: (commented)
+    // if (match_time_mode == "relive" && test_flash_finished_loaded(match_next_minute) && test_flash_played_minute(match_last_min)) {
+    if (match_data["report_done"].indexOf(match_next_minute) > -1 && match_time_mode == "relive" && test_flash_finished_loaded(match_next_minute) && test_flash_played_minute(match_last_min)) {
+        if (match_seconds < match_next_minute * 60) {
+            match_seconds = match_next_minute * 60;
+        } else {
+            match_seconds++;
+        }
+
+        // Allow clock to go
+    } else if ($.inArray(match_next_minute, flash_finished_loaded_minutes_ar) >= 0) {
+        match_seconds++;
+
+        // Disallow clock to pass match_next_minute until the clips are loaded for that minute
+    } else if (Math.floor((match_seconds + 1) / 60) < match_next_minute) {
+        match_seconds++;
+    }
+
+    setTimeout(run_match, 1000);
+
+}
+
+// AUX CODE REPLACE ORIGINAL FUNCTION
+
+addJS_Node(prepare_next_minute);
+addJS_Node(run_match);
+
+function addJS_Node(text, s_URL, funcToRun, runOnLoad) {
+    var D = document;
+    var scriptNode = D.createElement('script');
+    if (runOnLoad) {
+        scriptNode.addEventListener("load", runOnLoad, false);
+    }
+    scriptNode.type = "text/javascript";
+    if (text) scriptNode.textContent = text;
+    if (s_URL) scriptNode.src = s_URL;
+    if (funcToRun) scriptNode.textContent = '(' + funcToRun.toString() + ')()';
+
+    var targ = D.getElementsByTagName('head')[0] || D.body || D.documentElement;
+    targ.appendChild(scriptNode);
+}
+
+// SCRIPT CODE
+
+(function () {
+    'use strict';
+
+    var iframeReady = false;
+
+    if (window.top === window.self) {
+
+        // Here we are at the top window (TROPHYMANAGER) and we setup our message event listener, iframe and help info
+
+        jQuery('#main > div.abs.output.stats.text_center').append("No sound?<br>Select voice again on sound page once ---->");
+        jQuery('div.main_center').css('overflow', 'initial');
+        jQuery('<iframe />');
+        jQuery('<iframe />', {
+            name: 'frame1',
+            id: 'frame1',
+            style: 'position:absolute;left:320px;width:320px;height:600px',
+            src: 'https://www.naturalreaders.com/online/'
+        }).appendTo('#main > div.abs.output.stats.text_center');
+
+        var handleSizingResponse = function (e) {
+
+            if (e.origin == 'https://www.naturalreaders.com') {
+                var action = e.data.split(':')[0]
+                var message = e.data.split(':')[1]
+
+                if (!iframeReady) {
+                    if (action == 'status') {
+                        if (message == 'iframe.ready') {
+                            iframeReady = true;
+                            match_data["report_done"].push(0);
+                        }
+                    }
+                }
+
+                if (iframeReady) {
+                    if (action == 'game_play') {
+                        /**
+                         * Ended opting for forced timer
+                         */
+                        //var timerIn = parseInt(message);
+                        //if (timerIn >= 59000) {
+                        //    timerIn = 59000;
+                        //}
+
+                        timerIn = 55000;
+
+                        setTimeout(function () {
+                            match_data["report_done"].push(match_next_minute);
+                        }, timerIn);
+                    }
+
+                    //Lefted this comment here just because I'm awesome
+                    //iframe.contentWindow.postMessage('narrate:I'm awesome.', 'https://www.naturalreaders.com');
+                }
+            }
+        }
+
+        window.addEventListener('message', handleSizingResponse, false);
+
+
+    } else if (window.location.host == "www.naturalreaders.com") {
+
+        window.top.postMessage('status:iframe.ready', "*");
+        localStorage.setItem('NRPerson-input', '{"pageindex":0,"sentenceindex":0,"filetype":""}');
+        $("#inputDiv").html("");
+
+        var respondToSizingMessage = function (e) {
+            if (e.origin == 'https://trophymanager.com') { // e.data is the string sent by the origin with postMessage.
+
+                var action = e.data.split(':')[0]
+                var message = e.data.split(':')[1]
+
+
+
+                if (action == 'narrate') {
+                    $("#inputDiv").html(message);
+                    localStorage.setItem('NRPerson-input', '{"pageindex":0,"sentenceindex":0,"filetype":""}');
+                    $("#pw_wrapper > div.mainTop.clearfix > div.topNavlink > div.operateContent > app-reader > div.option.playPause.play").click();
+
+                    setTimeout(function () {
+                        window.top.postMessage('game_play:' + (4000 * $("#inputDiv > p").length), "*");
+                    }, 2000);
+
+                    // TODO: For relive game porpuses find a way to send message as soon voice ends
+
+                }
+            }
+        } // we have to listen for 'message'
+
+        window.addEventListener('message', respondToSizingMessage, false);
+    }
+})();
